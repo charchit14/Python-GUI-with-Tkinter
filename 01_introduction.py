f736bb97f3205cd55e0df3db547e80bb086e0049
Charchit <charchitregmi10@gmail.com>
2024-02-26 16:49:58 +0545

    Initializing

---
+++ 01_introduction.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+# Creating a base for GUI (creating an object of Tk() class)
+# This contains all the basic components needed for GUI
+root = Tk()
+
+# Starting the GUI logic
+root.mainloop()
