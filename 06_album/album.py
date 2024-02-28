86f312723d12e18f6d9bd3ffca80cd5273445fac
Charchit <charchitregmi10@gmail.com>
2024-02-28 19:33:18 +0545

    Album path fixed

--- 06_album/album.py
+++ 06_album/album.py
@@ -2,13 +2,12 @@ from tkinter import *
 
 root = Tk()
 
+# Inserting the text
 text_label = Label(text="This is my album")
 text_label.pack()
 
-
-
-img = PhotoImage(file="images\\photo-01.png")
-# img = PhotoImage(file="C:\Users\charc\Desktop\Python\Python GUI with Tkinter\06_album\images\photo-01.png")
+# Inserting images
+img = PhotoImage(file=r"C:\Users\charc\Desktop\Python\Python GUI with Tkinter\06_album\images\photo-01.png")
 img_label = Label(image=img)
 img_label.pack()
 
