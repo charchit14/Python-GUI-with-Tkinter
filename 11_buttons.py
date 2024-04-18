0335b74f32415e679a5a05a6d9fa849ebc77e406
Charchit <charchitregmi10@gmail.com>
2024-04-18 11:24:38 +0545

    Adding a button

--- 11_buttons.py
+++ 11_buttons.py
@@ -1,6 +1,15 @@
 from tkinter import *
 root = Tk()
 
+root.geometry("720x480")
+root.title("Buttons")
+
+f1 = Frame(root, bg="yellow", borderwidth=4, relief=GROOVE)
+f1.pack(side=TOP)
+
+b1 = Button(f1, fg="pink", text="This is the button")
+b1.pack()
+
 
 
 root.mainloop()
